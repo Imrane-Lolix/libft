81e1c740548b7ef29885e1e5a956b19dbe66d6e5
Imrane-Lolix <imranehalim2019@gmail.com>
2024-07-25 09:28:44 +0100

    added strncat

---
+++ ft_strncat.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+char	*ft_strncat(char *dst, const char *src, size_t n)
+{
+	size_t dst_len;
+	size_t src_len;
+
+	dst_len = ft_strlen(dst);
+	src_len = 0;
+	while (src[src_len] && src_len < n)
+		dst[dst_len++] = src[src_len++];
+	return (dst);
+}
--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "libft.h"
 #include "ft_strlen.c"
-#include "ft_strcat.c"
+#include "ft_strncat.c"
 
 int main() {
     const char	src[20] = "My name is Imrane\0";
     char		dst[40] = "Hello, ";
-	ft_strcat(dst, src);
+	ft_strncat(dst, src, 5);
 	printf("res = %s\n", dst);
     return 0;
 }
