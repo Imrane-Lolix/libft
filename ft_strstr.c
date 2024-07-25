42605dbfc241789a72a4c5f3298a5913b5faa7f6
Imrane-Lolix <imranehalim2019@gmail.com>
2024-07-25 13:18:06 +0100

    added strstr

---
+++ ft_strstr.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	size_t len;
+	size_t	i;
+
+	len = ft_strlen(needle);
+	i = 0;
+	while (haystack[i])
+	{
+		if (ft_strncmp(&haystack[i], needle, len) == 0)
+			return ((char *)&haystack[i]);
+		i++;
+	}
+	return (NULL);
+}
--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "libft.h"
 #include "ft_strncmp.c"
+#include "ft_strstr.c"
+#include "ft_strlen.c"
 
 int main() {
-    const char	*src = "aaabaa";
-    const char	*dst = "aaacaa";
+    const char	*src = NULL;
+    const char	*dst = "ba";
 	
-	printf("ptr    = %d\n", ft_strncmp(dst, src, 4));
-    printf("ptr    = %d\n", strncmp(dst, src, 4));
+	printf("ptr    = %p\n", ft_strstr(src, dst));
+    printf("ptr    = %p\n", strstr(src, dst));
 	return 0;
 }
