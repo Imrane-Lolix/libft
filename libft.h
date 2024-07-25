392106f81f3fd3fda7e930f39acd0e202f353a7f
Imrane-Lolix <imranehalim2019@gmail.com>
2024-07-25 17:58:22 +0100

    added other functions

--- libft.h
+++ libft.h
@@ -22,5 +22,17 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 // -- Conversion functions
 int		ft_atoi(const char *nptr);
 int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+// -- Char classification
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+
+// -- memory functions
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
 
 #endif
--- main.c
+++ main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-#include "libft.h"
-#include "ft_strncmp.c"
-#include "ft_strnstr.c"
-#include "ft_strlen.c"
-#include "ft_toupper.c"
 #include <ctype.h>
+#include "libft.h"
+#include "ft_memset.c"
+#include "ft_bzero.c"
+
 int main() {
-    //const char *n;
-	
-	printf("ptr    = %c\n", ft_toupper('S'));
-    printf("ptr    = %c\n", toupper('S'));
+    char n[10] = "aaaaaaaaaa";
+	ft_bzero(n, 10);
+	printf("ptr    = %s\n", n);
+    //printf("ptr    = %s\n", (char *)memset((void *)n, 65, 10));
 	return 0;
 }
